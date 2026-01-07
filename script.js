adba1df31b5c201358e997a959b4e6941e1e1487
Uttam Kumar <142337526+uttam5619@users.noreply.github.com>
2026-01-07 10:22:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
 // your code here
+const nameEle = document.getElementById('name')
+const yearEle = document.getElementById('year')
+const urlEle = document.getElementById('url')
+
+nameEle.addEventListener('input',(e)=>{
+	nameEle.value=e.target.value
+})
+
+yearEle.addEventListener('input',(e)=>{
+	yearEle.value=e.target.value
+})
+
+let url= urlEle.innerText
+
+const submitBtn= document.getElementById('button')
+submitBtn.addEventListener('click',function(){
+	const name= nameEle.value
+	const year= yearEle.value
+
+	if(name || url){
+		url+='?'
+	}
+
+	if(name){
+		let nameQuery = `name=${name}`
+		url+=nameQuery
+	}
+
+	if(year){
+		let yearQuery=`year=${year}`
+		if(name){
+			url+='&'
+		}
+		url+=yearQuery
+	}
+
+	urlEle.textContent=url
+})
